66e944129d3ca6c51f8199db54a235f007899ef2
Rahul <rahulprem02@gmail.com>
2025-09-20 20:52:27 +0200

    Implemented advanced HQL queries including DELETE, UPDATE, and JOIN operations

--- src/main/java/com/hql/HQLExample.java
+++ src/main/java/com/hql/HQLExample.java
@@ -2,6 +2,7 @@ package com.hql;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
@@ -16,13 +17,13 @@ public class HQLExample {
        SessionFactory factory = cgf.buildSessionFactory();
        
         Session s = factory.openSession();
-//HQL Syntax:
-       String query = "from Student where city = 'ABC'" ;
+        //HQL Syntax:
+        String query = "from Student where city = 'ABC'" ;
         Query q = s.createQuery(query);
         //Single (Unique result)
     
         //q.uniqueResult()
-        System.out.println(q.uniqueResult());
+        System.out.println("Pakistan"+ q.uniqueResult());
         //Multiple List
         // List<Student> list =  q.uniqueResult();
 
@@ -31,8 +32,25 @@ public class HQLExample {
         //     System.out.println(student.getName());
         // }
 
+// Delete Query
+        Transaction tx = s.beginTransaction();
+        // Query q2  = s.createQuery("delete from Student where city = 'ABC'");
 
-       s.close();
-       factory.close();
+        // int r = q2.executeUpdate();
+        // System.out.println("Deleted");
+        // System.out.println(r);
+    
+//Update Query
+        Query q2 = s.createQuery("Update Student set city=:c where name=:n");
+        q2.setParameter("c","Delhi");
+        q2.setParameter("n","Peter");
+        int r = q2.executeUpdate();
+        System.out.println(r + "Object Updated");
+        
+
+        tx  .commit();
+        s.close();
+        factory.close();
+
     }
 }
