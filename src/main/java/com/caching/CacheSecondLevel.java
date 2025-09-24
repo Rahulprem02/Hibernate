ad5b9ae6300a3b33fe3ab2be28bf6e89ab88733e
Rahul <rahulprem02@gmail.com>
2025-09-24 23:08:34 +0200

    Hibernate Cache and Second-Level Cache

--- src/main/java/com/caching/CacheSecondLevel.java
+++ src/main/java/com/caching/CacheSecondLevel.java
@@ -7,6 +7,7 @@ import org.hibernate.cfg.Configuration;
 import com.tut.Student;
 
 public class CacheSecondLevel {
+
     public static void main(String[] args) {
       SessionFactory factory =    new Configuration().configure().buildSessionFactory();
       Session session = factory.openSession();
@@ -14,7 +15,7 @@ public class CacheSecondLevel {
       Student student = session.get(Student.class, 12424);
       System.out.println(student);
 
-      System.out.println("Working student");
+      System.out.println("Working student ");
 
       Student student1 = session.get(Student.class, 12424);
       System.out.println(student1);
